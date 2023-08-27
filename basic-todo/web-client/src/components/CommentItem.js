d6fc6484bf8ae48aab0d1815c7c4158584c2f0a4
akshayp-mangospring <akshayp@mangospring.com>
2023-08-27 09:18:38 +0530

    Add Replies on Comments on the Front End with Dummy data

--- basic-todo/web-client/src/components/CommentItem.js
+++ basic-todo/web-client/src/components/CommentItem.js
@@ -10,7 +10,9 @@ function Comment({ comment, author, hasEditRights, embeddedParent, setComments, 
   const parentId = embeddedParent.id;
   const commentId = comment.id;
   const inputRef = useRef(null);
+  const hasReplies = false;
   const [isEditMode, setIsEditMode] = useState(false);
+  const [shouldShowReplies, setShouldShowReplies] = useState(false);
   const [editValue, setEditValue] = useState(comment.content);
 
   useEffect(() => {
@@ -70,42 +72,77 @@ function Comment({ comment, author, hasEditRights, embeddedParent, setComments, 
   };
 
   return (
-    <div className="mb-1 d-flex align-items-center">
-      <span className="fw-medium me-3">{author.username}:</span>
-      {
-        isEditMode ? (
-          <>
-            <input
-              type="text"
-              ref={inputRef}
-              className="w-100 form-control me-auto"
-              value={editValue}
-              onChange={(e) => setEditValue(e.target.value)}
-              onKeyDown={handleKeyDown}
-            />
-            <a href={DEAD_LINK} onClick={editComment} className="d-flex align-items-center text-success ms-3">
-              <Tick />
-            </a>
-            <a href={DEAD_LINK} onClick={() => setIsEditMode(false)} className="d-flex align-items-center text-danger ms-3">
-              <Close />
-            </a>
-          </>
-        ) : (
-          <>
-            <span className="me-auto">{comment.content}</span>
-            {hasEditRights && (
-              <div className="d-flex align-items-center">
-                <a href={DEAD_LINK} onClick={() => setIsEditMode(true)} className="d-flex align-items-center text-primary">
-                  <Pen />
-                </a>
-                <a href={DEAD_LINK} onClick={() => deleteComment(comment.id)} className="d-flex align-items-center text-danger ms-3">
-                  <Trash />
-                </a>
+    <div className="mb-2">
+      <div className="d-flex align-items-center">
+        <span className="fw-medium me-3">{author.username}:</span>
+        {
+          isEditMode ? (
+            <>
+              <input
+                type="text"
+                ref={inputRef}
+                className="w-100 form-control me-auto"
+                value={editValue}
+                onChange={(e) => setEditValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+              <a href={DEAD_LINK} onClick={editComment} className="d-flex align-items-center text-success ms-3">
+                <Tick />
+              </a>
+              <a href={DEAD_LINK} onClick={() => setIsEditMode(false)} className="d-flex align-items-center text-danger ms-3">
+                <Close />
+              </a>
+            </>
+          ) : (
+            <>
+              <span className="me-auto">{comment.content}</span>
+              {hasEditRights && (
+                <div className="d-flex align-items-center">
+                  <a href={DEAD_LINK} onClick={() => setIsEditMode(true)} className="d-flex align-items-center text-primary">
+                    <Pen />
+                  </a>
+                  <a href={DEAD_LINK} onClick={() => deleteComment(comment.id)} className="d-flex align-items-center text-danger ms-3">
+                    <Trash />
+                  </a>
+                </div>
+              )}
+            </>
+          )
+        }
+      </div>
+      <div className="mb-2">
+        <span className="text-primary" role="button" onClick={() => setShouldShowReplies(!shouldShowReplies)}>
+          Replies
+        </span>
+      </div>
+      <div className="ms-3">
+        {
+          shouldShowReplies && (
+            <>
+              <div className="mb-2">
+                {
+                  hasReplies ? (
+                    <>
+                      {
+                        [{ username: 'abp437', content: 'This is dummy reply', }, { username: 'sanketsheth', content: 'Main Badla Loonga', },].map(({ username, content }) => (
+                          <div>
+                            <span className="fw-medium me-3">{username}:</span>
+                            <span className="me-3">{content}</span>
+                          </div>
+                        ))
+                      }
+                    </>
+                  ) : (<span>No replies</span>)
+                }
               </div>
-            )}
-          </>
-        )
-      }
+              <div className="input-group input-group-md mb-3">
+                <input type="text" className="form-control" placeholder="Add your reply here..." value="" />
+                <button type="button" className="btn btn-primary disabled">Add</button>
+              </div>
+            </>
+          )
+        }
+      </div>
     </div>
   );
 }
